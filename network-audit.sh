763cf7e342e2855f03b17303d5849ab97ca27345
Sriram Ramanujam <sriram.researcher@gmail.com>
2020-04-19 11:41:01 +0530

    Added SNMP and CLOGIN LOGics

--- network-audit.sh
+++ network-audit.sh
@@ -24,11 +24,25 @@ do
 	echo "$input,$PING_STATUS"
 
 	### SNMP TEST
+	
+	HOSTNAME=`/usr/bin/snmpwalk -v 2c -c public $input iso.3.6.1.2.1.1.5.0`
+	echo "$HOSTNAME"
+
+	CPU_UTILS=`/usr/bin/snmpwalk -v 2c -c public $input 1.3.6.1.4.1.9.2.1.58`
+	echo "CPU_UTILS"
+
+	if [[ -n $HOSTNAME ]] && [[ -n $CPU_UTILS ]]; then
+		SNMP_STATUS="YES"
+	else
+		SNMP_STATUS="NO"
+	fi
+
+	echo "$input,$PING_STATUS,$SNMP_STATUS"
 
 	### CLOGIN TEST
 
 	CLOGIN_OUTPUT=`./clogin -c "" $input`
-	echo "CLOGIN_OUTPUT"
+	echo "$CLOGIN_OUTPUT"
 
 	sleep 1
 
