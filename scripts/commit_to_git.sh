7be9aafa9bcf8627daa882c2d1943f9e3be762da
sriram.researcher@gmail.com <sriram>
2020-05-04 14:11:33 +0530

    Mon May  4 14:11:33 IST 2020

--- scripts/commit_to_git.sh
+++ scripts/commit_to_git.sh
@@ -3,6 +3,9 @@
 
 cd /home/tools/network-assess/coolcode19042020
 
+git config --global user.email "sriram"
+git config --global user.name "sriram.researcher@gmail.com"
+
 git add *
 
 git commit -m "`date`"
